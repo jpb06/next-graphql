4bd92cf7a26a329cafbd04e88ff09d5f1ff98b78
jpb06 <jpb.06@outlook.com>
2024-05-07 22:41:50 +0200

    init

---
+++ codegen.ts
@@ -0,0 +1,18 @@
+import type { CodegenConfig } from '@graphql-codegen/cli';
+
+const config: CodegenConfig = {
+  overwrite: true,
+  schema: 'https://graphql-shop-api.fly.dev/graphql',
+  documents: ['src/**/*.ts', 'src/**/*.tsx'],
+  generates: {
+    'src/graphql/codegen/': {
+      preset: 'client',
+      plugins: [],
+    },
+    'src/graphql/codegen/graphql.schema.json': {
+      plugins: ['introspection'],
+    },
+  },
+};
+
+export default config;
