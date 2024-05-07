35cd24edfb47f4b492a73ded315ff81e571119e4
jpb06 <jpb.06@outlook.com>
2024-05-07 22:47:55 +0200

    yasman

--- src/components/products/ProductsByQuery.tsx
+++ src/components/products/ProductsByQuery.tsx
@@ -4,13 +4,17 @@ import { useQuery } from '@tanstack/react-query';
 
 import { getAllProducts } from '../../graphql/queries/products.graphql';
 
+import { ProductsByPropsDrilling } from './ProductsByPropsDrilling';
+
 export const ProductsByQuery = () => {
   const { data } = useQuery({
     queryKey: ['products'],
     queryFn: getAllProducts,
   });
 
-  return (
-    <ul>{data?.products.map(({ id, name }) => <li key={id}>{name}</li>)}</ul>
-  );
+  if (!data?.products) {
+    return <div>Loading</div>;
+  }
+
+  return <ProductsByPropsDrilling data={data?.products} />;
 };
